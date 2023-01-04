029bef6fd6203f04a7bc94a4559e270c258528c2
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-04 13:10:10 +0530

    updateing state from other component usind redux dispatch function

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,22 @@
-import redux from "redux";
+import { legacy_createStore as createStore } from 'redux';
 
 
-const counterReducer = (state = {count: 3}, action)=>{
+const counterReducer = (state = {counter: 0}, action)=>{
 	if (action.type === "increment") {
 		return{
-			count: state.count + 1
+			counter: state.counter + 1
 		}
 	}
 	
 	if (action.type === "decrement") {
 		return{
-			count: state.count - 1
+			counter: state.counter - 1
 		}
 	}
 	return state
 }
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 
 console.log(store.getState());
